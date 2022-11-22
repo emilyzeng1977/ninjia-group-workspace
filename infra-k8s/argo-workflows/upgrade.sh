5cb4aa24dcbbc868c5f84e0bb1b52fbe19ade4a1
emily <tom.niu8@gmail.com>
2022-11-22 11:39:57 +1100

    checkout code

--- infra-k8s/argo-workflows/upgrade.sh
+++ infra-k8s/argo-workflows/upgrade.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 # kubectl kustomize .
-#echo -n ${AWS_ACCESS_KEY_ID_VAL} > ./permissions/AWS_ACCESS_KEY_ID
-#echo -n ${AWS_SECRET_ACCESS_KEY_VAL} > ./permissions/AWS_SECRET_ACCESS_KEY
-#echo -n ${AWS_DEFAULT_REGION_VAL} > ./permissions/AWS_DEFAULT_REGION
+echo -n ${AWS_ACCESS_KEY_ID_VAL} > ./permissions/AWS_ACCESS_KEY_ID
+echo -n ${AWS_SECRET_ACCESS_KEY_VAL} > ./permissions/AWS_SECRET_ACCESS_KEY
+echo -n ${AWS_DEFAULT_REGION_VAL} > ./permissions/AWS_DEFAULT_REGION
 kubectl apply -k .
-#rm ./permissions/AWS_ACCESS_KEY_ID
-#rm ./permissions/AWS_SECRET_ACCESS_KEY
-#rm ./permissions/AWS_DEFAULT_REGION
+rm ./permissions/AWS_ACCESS_KEY_ID
+rm ./permissions/AWS_SECRET_ACCESS_KEY
+rm ./permissions/AWS_DEFAULT_REGION
 kubectl -n argo port-forward deployment/argo-server 2746:2746
