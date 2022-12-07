bbf0c2f7c084df1c5710ed6dce5c93916bd0b642
tomniu <tniu@infomedia.com.au>
2022-12-08 07:39:46 +1100

    secret demo

--- infra-k8s/argo-workflows/upgrade.sh
+++ infra-k8s/argo-workflows/upgrade.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 # kubectl kustomize .
+export AWS_ACCESS_KEY_ID_VAL="access_key_id_123"
+export AWS_SECRET_ACCESS_KEY_VAL="secret_access_key_123"
+export AWS_DEFAULT_REGION_VAL="default_region_123"
 echo -n ${AWS_ACCESS_KEY_ID_VAL} > ./permissions/AWS_ACCESS_KEY_ID
 echo -n ${AWS_SECRET_ACCESS_KEY_VAL} > ./permissions/AWS_SECRET_ACCESS_KEY
 echo -n ${AWS_DEFAULT_REGION_VAL} > ./permissions/AWS_DEFAULT_REGION
