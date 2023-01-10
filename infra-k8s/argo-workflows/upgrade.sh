bf9b1cd16593211fba4f140e6d78c436d894399d
emily <tom.niu8@gmail.com>
2023-01-10 17:15:13 +1100

    ingress

--- infra-k8s/argo-workflows/upgrade.sh
+++ infra-k8s/argo-workflows/upgrade.sh
@@ -9,4 +9,5 @@ rm ./permissions/AWS_ACCESS_KEY_ID
 rm ./permissions/AWS_SECRET_ACCESS_KEY
 rm ./permissions/AWS_DEFAULT_REGION
 echo "hello"
-kubectl -n argo port-forward deployment/argo-server 2746:2746
+#kubectl -n argo port-forward deployment/argo-server 2746:2746
+kubectl -n argo port-forward service/argo-server 2746:2746
