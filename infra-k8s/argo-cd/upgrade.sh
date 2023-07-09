3997278943c348ddebe27f925a0d11bc8220fcac
emily <tom.niu8@gmail.com>
2023-07-09 10:20:49 +1000

    rollouts

--- infra-k8s/argo-cd/upgrade.sh
+++ infra-k8s/argo-cd/upgrade.sh
@@ -10,3 +10,10 @@ sleep 5
 kubectl port-forward svc/argocd-server -n argocd 8088:443
 # obtain pwd for admin
 # kubectl get pod -n argocd |grep argocd-server
+
+# Guide to learn rollouts
+# https://wghdr.top/archives/1724
+# brew install argoproj/tap/kubectl-argo-rollouts
+
+# https://www.eksworkshop.com/docs/automation/gitops/
+# https://catalog.workshops.aws/eks-blueprints-terraform/en-US/060-argo-rollout/1-enable-argo-rollout
